54749104df6bacc0508a4be06ec0c6744c858c59
Daniel Day <danielday@Daniels-MacBook-Pro.local>
2017-08-27 19:09:38 -0400

    final

--- BasicCard.js
+++ BasicCard.js
@@ -1,41 +1,29 @@
 var fs = require("fs");
-
-// fs.appendFile('log.txt', ' This is my text.', function (err) {
-//   if (err) throw err;
-//   console.log('Updated!');
-// });
-
 module.exports = BasicCard;
 
-// constructor for BasicFlashcard
 function BasicCard(front, back) {
-    console.log("BasicFlashcard");
-    console.log("front" + front);
-    console.log("back" + back);
+
+    console.log("Basic Flashcard");
+    console.log("front: " + front);
+    console.log("back: " + back);
+
     this.front = front;
     this.back = back;
-    // this.create = function() {
-    console.log("works");
-    // // flashcard object to be appended to file
+
     var data = {
         front: this.front,
         back: this.back,
-        // type: "basic"
+        // newLine: "\n"
+
     };
-    // add card to log.txt
-    fs.appendFile('log.txt', JSON.stringify(data) + ';', 'utf8', function(err) {
-        // fs.appendFile("log.txt", "test", function(error) {
 
-        // fs.appendFile('log.txt', ' This is my text.', function (err) {
-        // if (err) throw err;
+    fs.appendFile('log.txt', JSON.stringify(data) + '\n', 'utf8', function(err) {
 
-
-        // if there is an error, log the error
         if (err) {
             console.log("error");
             console.log(err);
 
         };
     });
-    // };
+
 };
